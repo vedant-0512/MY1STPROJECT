9deedc8c756d1954fd3ddb2ccfd580f4f0f368bc
vedant bhakre <vedantbhakre07@gmail.com>
2025-11-28 00:43:09 +0530

    my hobbies

--- arrays.py
+++ arrays.py
@@ -1,4 +1,3 @@
-number=int(input("enter a number"))
+number=int(input("enter a number")5)
 double = number * 2
 print(f"double of number is {double}")
-5
---
+++ my_hobbis.py
@@ -0,0 +1,3 @@
+hobbies=["reading","swimming","coding","travelling"]
+for x in hobbies:
+    print(f"i like {x}")
